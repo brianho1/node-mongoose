e83d575c590e3b46b7702a95465f7a6aac071836
hobr86 <hobr86@gmail.com>
2021-03-20 03:00:14 -0500

    node mongoose part2

--- index.js
+++ index.js
@@ -8,21 +8,35 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthappiza',
+        name: 'Uthappiza2',
         description: 'test'
     })
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-        console.log(dishes);
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: "Updated test" }
+            },
+                {
+                    new: true
+                }).exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating: 5,
+                comment: "I love this dish",
+                author: 'brian aho'
+            })
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish)
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
